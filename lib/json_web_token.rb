9ee4275dc2dadf212e18f96d9944ddbf482c80d2
saurabh <saurabh@cardup.co>
2020-02-20 21:30:17 +0530

    use crentials instead of secrets

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,11 +4,11 @@ class JsonWebToken
   
   def self.encode(payload)
     payload.reverse_merge!(meta)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, CrooshApi::Application.credentials.secret_key_base)
   end
 
   def self.decode(payload)
-    JWT.decode(payload, Rails.application.secrets.secret_key_base)
+    JWT.decode(payload, CrooshApi::Application.credentials.secret_key_base)
   end
 
   def self.valid_payload?(payload)

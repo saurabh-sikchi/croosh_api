cc6375f5d7427f15347a855327725c1a3af9c48f
saurabh <saurabh@cardup.co>
2020-02-23 11:10:22 +0530

    user profile api endpoint changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::UsersController < ApplicationController
         h[croosh.id] = {
           video_url: video_url,
           total_likes_count: croosh.user_likes_count + croosh.celeb_likes_count,
-          date_delivered: croosh.created_at,
+          date: croosh.created_at,
           celeb_profile_pic: croosh.celeb.profile_pic,
           thumbnail: thumbnail_url
         }

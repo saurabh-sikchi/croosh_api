4b3631b3b0678a45ef1f38e3642b4ccf084c48b1
saurabh <saurabh@cardup.co>
2020-03-08 00:43:22 +0530

    change user profile api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,24 +3,39 @@ class Api::V1::UsersController < ApplicationController
 
   def profile
     crooshes = @current_user.crooshes
-    data = [].tap do |h|
-      crooshes.each do |croosh|
-        video_url = asset_url(croosh.video)
-        thumbnail_url = asset_url(croosh.thumbnail)
-        updates = croosh.croosh_updates.map(&:update_text)
-        h.push([
-          croosh_id: croosh.id,
-          video_url: video_url,
-          total_likes_count: croosh.user_likes_count + croosh.celeb_likes_count,
-          date: croosh.created_at.strftime('%b %-d, %Y'),
-          celeb_profile_pic: asset_url(croosh.celeb.profile_pic),
-          thumbnail: thumbnail_url,
-          updates: updates,
-          is_request: croosh.is_request?,
-        ])
-      end
+    # data = [].tap do |h|
+    #   crooshes.each do |croosh|
+    #     video_url = asset_url(croosh.video)
+    #     thumbnail_url = asset_url(croosh.thumbnail)
+    #     updates = croosh.croosh_updates.map(&:update_text)
+    #     h.push([
+    #       croosh_id: croosh.id,
+    #       video_url: video_url,
+    #       total_likes_count: croosh.user_likes_count + croosh.celeb_likes_count,
+    #       date: croosh.created_at.strftime('%b %-d, %Y'),
+    #       celeb_profile_pic: asset_url(croosh.celeb.profile_pic),
+    #       thumbnail: thumbnail_url,
+    #       updates: updates,
+    #       is_request: croosh.is_request?,
+    #     ])
+    #   end
+    # end
+    crooshes_ar = crooshes.inject([]) do |h, croosh|
+      video_url = asset_url(croosh.video)
+      thumbnail = asset_url(croosh.thumbnail)
+      updates = croosh.croosh_updates.map(&:update_text)
+      h.push({
+        croosh_id: croosh.id,
+        total_likes_count: croosh.user_likes_count + croosh.celeb_likes_count,
+        thumbnail: thumbnail,
+        date: croosh.created_at.strftime('%b %-d, %Y'),
+        video_url: video_url,
+        updates: updates,
+        is_request: croosh.is_request?,
+        celeb_profile_pic: asset_url(croosh.celeb.profile_pic),
+      })
     end
-    render json: { user_name: @current_user.name, crooshes: data }, status: 200
+    render json: { user_name: @current_user.name, crooshes: crooshes_ar }, status: 200
   end
 
   def change_profile_pic
